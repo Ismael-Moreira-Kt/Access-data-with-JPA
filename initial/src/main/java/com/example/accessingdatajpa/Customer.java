612c541b3997689e7577801fd44bf3b23ff5b5fc
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-03-02 20:07:05 +0000

    Feat: Customer Repository created

--- initial/src/main/java/com/example/accessingdatajpa/Customer.java
+++ initial/src/main/java/com/example/accessingdatajpa/Customer.java
@@ -6,16 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Costumer {
+public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
 
-    protected Costumer() {}
+    protected Customer() {}
 
-    public Costumer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
